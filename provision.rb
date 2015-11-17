c1babc3de1c4ae3d43b620150761a7549ec3bf1c
jvillarejo <arzivian87@gmail.com>
2015-11-17 17:19:22 -0300

    provisioning for mongodb

--- provision.rb
+++ provision.rb
@@ -1,10 +1,11 @@
 require 'dotenv'
-require 'aws-sdk'
 
 Dotenv.load
 
-s3 = Aws::S3::Client.new
-resp = s3.list_buckets
-puts resp.buckets.map(&:name)
+username = ENV['USERNAME']
+hosto = ENV['HOST']
 
+ssh = "#{username}@#{host}"
 
+system("rsync -avzP ./setup/files #{ssh}:~/tmp")
+system("ssh #{ssh} bash -s < ./setup/provision.sh")
---
+++ setup/provision.sh
@@ -0,0 +1,9 @@
+#/bin/bash
+sudo apt-get -y update
+sudo apt-get -y upgrade
+
+sudo apt-get -y install mongodb
+sudo cp ~/tmp/files/mongodb.conf /etc/mongodb.conf
+sudo service mongodb restart
+
+rm -rf ~/tmp
