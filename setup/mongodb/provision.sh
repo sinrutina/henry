85b3cb061314c5a4fe8e931bfaaf794ab74c0235
jvillarejo <arzivian87@gmail.com>
2015-11-18 13:04:25 -0300

    better provision

--- setup/mongodb/provision.sh
+++ setup/mongodb/provision.sh
@@ -1,7 +1,9 @@
 #/bin/bash
-sudo apt-get -y update
-sudo apt-get -y upgrade
-sudo apt-get -y install mongodb
+sudo apt-get -y update >/dev/null 2>&1
+sudo apt-get -y upgrade >/dev/null 2>&1
+
+sudo apt-get -y install mongodb >/dev/null 2>&1
 sudo cp ~/tmp/files/mongodb.conf /etc/mongodb.conf
 sudo service mongodb restart
+
 rm -rf ~/tmp
--- setup/redis/provision.sh
+++ setup/redis/provision.sh
@@ -1,7 +1,12 @@
 #/bin/bash
-sudo apt-get -y update
-sudo apt-get -y upgrade
-sudo apt-get -y install redis-server
+echo "Upgrading system..."
+sudo apt-get -y update >/dev/null 2>&1
+sudo apt-get -y upgrade >/dev/null 2>&1
+
+echo "Installing redis..."
+sudo apt-get -y install redis-server >/dev/null 2>&1
 sudo cp ~/tmp/files/redis.conf /etc/redis/redis.conf
 sudo service redis-server restart
+
+echo "Removing temp files..."
 rm -rf ~/tmp
