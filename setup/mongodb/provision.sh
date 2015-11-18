811e3ddfbce1d54a69027ff4e8dccd77a57ed227
jvillarejo <arzivian87@gmail.com>
2015-11-18 11:58:05 -0300

    fixing provisioning

--- setup/mongodb/provision.sh
+++ setup/mongodb/provision.sh
@@ -1,9 +1,7 @@
 #/bin/bash
 sudo apt-get -y update
 sudo apt-get -y upgrade
-
 sudo apt-get -y install mongodb
 sudo cp ~/tmp/files/mongodb.conf /etc/mongodb.conf
 sudo service mongodb restart
-
 rm -rf ~/tmp
--- setup/redis/provision.sh
+++ setup/redis/provision.sh
@@ -1,9 +1,7 @@
 #/bin/bash
 sudo apt-get -y update
 sudo apt-get -y upgrade
-
 sudo apt-get -y install redis-server
 sudo cp ~/tmp/files/redis.conf /etc/redis/redis.conf
 sudo service redis-server restart
-
 rm -rf ~/tmp
