83a8fc091775bbc21860da3507ff433a57d37e6a
jvillarejo <arzivian87@gmail.com>
2015-11-18 15:32:27 -0300

    extracted with erb

--- provision.rb
+++ provision.rb
@@ -1,6 +1,12 @@
+require 'erb'
 require 'dotenv'
+require 'fileutils'
 
 Dotenv.load
+
+def render(path,context=nil)
+  ERB.new(File.read(path)).result(context)
+end
 
 assembly_line_name = ARGV[0] or raise 'you must describe an assembly line'
 ssh = ARGV[1] or raise 'you must describe a server to assemble'
@@ -9,9 +15,13 @@ assembly_line = "./setup/#{assembly_line_name}"
 
 Dir.exist?(assembly_line) or raise 'assembly line doesn\'t exist'
 
-puts "Provisioning #{assembly_line_name} on #{ssh}..."
+rendered = render("#{assembly_line}/provision.sh.erb",binding)
+
+FileUtils.mkdir_p('./compiled')
+File.open('./compiled/provision.sh','w') { |f| f.write(rendered) }
 
 output = "rsync -avzP #{assembly_line}/files #{ssh}:~/tmp;"\
-         "ssh #{ssh} bash -s < #{assembly_line}/provision.sh"
+         "ssh #{ssh} bash -s < ./compiled/provision.sh"
 
+puts "Provisioning #{assembly_line_name} on #{ssh}..."
 system(output)
