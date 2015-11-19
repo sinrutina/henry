c07fcb1f818d4926c06f46c18626e1f65b6f38e6
jvillarejo <arzivian87@gmail.com>
2015-11-19 13:03:25 -0300

    added conf and refactored

--- provision.rb
+++ provision.rb
@@ -8,21 +8,46 @@ def render(path,context=nil)
   ERB.new(File.read(path)).result(context)
 end
 
+def compile_conf_files(assembly_line, context=nil)
+  conf_files_dir = "#{assembly_line}/files"
+
+  Dir.foreach(conf_files_dir) do |file_name|
+    next if file_name == '.' or file_name == '..'
+    rendered = render("#{conf_files_dir}/#{file_name}",context)
+    output_file_name = file_name.gsub('.erb','')
+    File.open("./compiled/files/#{output_file_name}",'w') { |f| f.write(rendered) }
+  end
+end
+
+def compile_main_script(assembly_line, context=nil)
+  rendered = render("#{assembly_line}/provision.sh.erb",context)
+  File.open('./compiled/provision.sh','w') { |f| f.write(rendered) }
+end
+
+def create_compiled_folder
+  FileUtils.rm_rf('./compiled')
+  FileUtils.mkdir_p('./compiled/files')
+end
+
+def compile_files(assembly_line, context=nil)
+  create_compiled_folder
+  compile_conf_files(assembly_line,context)
+  compile_main_script(assembly_line,context)
+end
+
 assembly_line_name = ARGV[0] or raise 'you must describe an assembly line'
 ssh = ARGV[1] or raise 'you must describe a server to assemble'
+server_name = ssh.split('@')[1]
 
 assembly_line = "./setup/#{assembly_line_name}"
 
 Dir.exist?(assembly_line) or raise 'assembly line doesn\'t exist'
 
-rendered = render("#{assembly_line}/provision.sh.erb",binding)
-
-FileUtils.mkdir_p('./compiled')
-File.open('./compiled/provision.sh','w') { |f| f.write(rendered) }
+compile_files(assembly_line,binding)
 
 output = "echo \"Copying temp files...\";"\
-         "rsync -avzP #{assembly_line}/files #{ssh}:~/tmp > /dev/null;"\
+         "rsync -avzP compiled/files #{ssh}:~/tmp > /dev/null;"\
          "ssh #{ssh} sudo bash -s < ./compiled/provision.sh"
 
 puts "Provisioning #{assembly_line_name} on #{ssh}..."
-system(output)
+#system(output)
